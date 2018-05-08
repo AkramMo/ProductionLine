9b60f06241de63574d58ecca9e57c17b80a10bbb
akrammo <ak4am.mo@gmail.com>
2018-05-08 14:36:13 -0400

    Initialisation of the project production line
    Laboratory 1

---
+++ src/simulation/FenetrePrincipale.java
@@ -0,0 +1,40 @@
+package simulation;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JFrame;
+
+public class FenetrePrincipale extends JFrame implements PropertyChangeListener {
+
+	private static final long serialVersionUID = 1L;
+	private static final String TITRE_FENETRE = "Laboratoire 1 : LOG121 - Simulation";
+	private static final Dimension DIMENSION = new Dimension(700, 700);
+
+	public FenetrePrincipale() {
+		PanneauPrincipal panneauPrincipal = new PanneauPrincipal();
+		MenuFenetre menuFenetre = new MenuFenetre();
+		add(panneauPrincipal);
+		add(menuFenetre, BorderLayout.NORTH);
+		// Faire en sorte que le X de la fenêtre ferme la fenêtre
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle(TITRE_FENETRE);
+		setSize(DIMENSION);
+		// Rendre la fenêtre visible
+		setVisible(true);
+		// Mettre la fenêtre au centre de l'écran
+		setLocationRelativeTo(null);
+		// Empêcher la redimension de la fenêtre
+		setResizable(false);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("TEST")) {
+			repaint();
+			System.out.println(evt.getNewValue());
+		}
+	}
+}
